b6bf1492f53b6534be74aa8544de399d525f42af
sarahelshabrawy <hylrcs@gmail.com>
2020-11-21 21:06:40 +0200

    Final

--- src/main/java/com/example/Calculator/controller/Controller.java
+++ src/main/java/com/example/Calculator/controller/Controller.java
@@ -1,19 +1,17 @@
 package com.example.Calculator.controller;
 
 import com.example.Calculator.model.Expression;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-
+@CrossOrigin
 @RestController
 public class Controller {
     @GetMapping("/calculate")
     public String Calculate(@RequestParam(value = "expression",defaultValue = "0")String expression){
         Expression ex = new Expression();
-        expression = URLDecoder.decode(expression, StandardCharsets.UTF_8);
         return ex.resolveExpression(expression);
     }
 }
--- src/main/java/com/example/Calculator/model/Expression.java
+++ src/main/java/com/example/Calculator/model/Expression.java
@@ -1,7 +1,9 @@
 package com.example.Calculator.model;
 
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class Expression{
 
@@ -10,44 +12,52 @@ public class Expression{
         StringBuilder num1 = new StringBuilder();
         StringBuilder num2 = new StringBuilder();
 
-        String[] split = expression.trim().split("");
-
+        String[] split = expression.trim().split("\\s+");
         for (String s : split) {
             if (s.matches("-?\\d+(\\.\\d+)?")) {
                 if(operation=='#')
                     num1.append(s);
                 else num2.append(s);
-            } else if (s.matches("[-/+*^%]")) {
-                if(operation=='#') {
+            } else if (s.matches("[-÷+x^%]")) {
+                if(operation=='#'||operation == '-') {
                     operation = s.toCharArray()[0];
                 }
                 else
                     return "E";
             }
-            else if(!s.matches("\\s+"))
+            else if(s.contains("e+")){
+                //for huge numbers, handling the exponential symbol
+                String[] pow = s.split("e+");
+                s = String.valueOf(Double.parseDouble(pow[0]) * Math.pow(10, Double.parseDouble(pow[1])));
+                if(operation=='#')
+                    num1.append(s);
+                else num2.append(s);
+            }
+            else
                 return "E";
         }
         if(operation=='#')
             return num1.toString();
-        if((operation == '/'|| operation == '%')&& num2.toString().equals("0"))
+        else if(num2.length() == 0 || num1.length()==0)
             return "E";
-        return String.valueOf(operation(Double.parseDouble(num1.toString()),Double.parseDouble(num2.toString()),operation));
+        if((operation == '÷'|| operation == '%')&& num2.toString().equals("0"))
+            return "E";
+
+        return String.valueOf(
+                operation(Double.parseDouble(num1.toString())
+                        ,Double.parseDouble(num2.toString())
+                        , operation));
     }
     private double operation(double a,double b ,char operation){
         switch (operation){
             case '+' : return a+b;
             case '-' : return a-b;
-            case '*' : return a*b;
-            case '/' : return a/b;
+            case 'x' : return a*b;
+            case '÷' : return a/b;
             case '^' : return Math.pow(a,b);
             case '%' : return a%b;
         }
         return 0;
     }
 
-    public static void main(String[] args) {
-        Expression ex = new Expression();
-        System.out.println(ex.resolveExpression("8+9"));
-        System.out.println(URLEncoder.encode("6 % 9", StandardCharsets.UTF_8));
-    }
 }
